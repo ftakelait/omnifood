23ad64bf3ef9756fad7d0f8edf6e3a172bb6f60a
Fouzi Takelait <ftakelait@gmail.com>
2025-03-13 18:48:45 -0500

    updated

--- auto_push.sh
+++ auto_push.sh
@@ -8,7 +8,7 @@ while true; do
     # Check if there is anything to commit
     if ! git diff-index --quiet HEAD --; then
         # Commit with a timestamp message (adjust branch name if needed)
-        git commit -m "Commit on $(date)"
+        git commit -m "Last update $(date)"
         git push origin main
     else
         echo "No changes to commit at $(date)"
